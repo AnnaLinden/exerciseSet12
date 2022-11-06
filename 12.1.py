bce6721e6cd89a6ff6b3874b1e2d5ed58a43f3ca
AnnaLinden <linden.a.anna@gmail.com>
2022-11-06 20:27:19 +0200

    Initial commit

---
+++ 12.1.py
@@ -0,0 +1,12 @@
+# Write a program that fetches and prints out a random Chuck Norris joke for the user. Use the API presented here:
+# https://api.chucknorris.io/. The user should only be shown the joke text.
+
+import requests
+import json
+
+request = "https://api.chucknorris.io/jokes/random"
+response = requests.get(request).json()
+print(json.dumps(response, indent=2))
+print(f"{response['value']}")
+#for a in response:
+#   print(a["value"])
---
+++ 12.2.py
@@ -0,0 +1,28 @@
+# Familiarize yourself with the OpenWeather weather API at: https://openweathermap.org/api. Your task is to write a
+# program that asks the user for the name of a municipality and then prints out the corresponding weather condition
+# description text and temperature in Celsius degrees. Take a good look at the API documentation. You must register
+# for the service to receive the API key required for making API requests. Furthermore, find out how you can convert
+# Kelvin degrees into Celsius.
+
+import requests
+
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+
+def get_city_name():
+    city = input("Enter the name of the city: ")
+    return city
+
+
+def get_temp_in_kelvin(city):
+    request = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=0ed541848a79ebc81ee98bea04f8942b"
+    response = requests.get(request).json()
+    kelvin = response['main']['temp']
+    return kelvin
+
+
+city = get_city_name()
+celsius = kelvin_to_celsius(get_temp_in_kelvin(city))
+print(f"The temperature in {city} is {round(celsius, 1)} degrees Celsius.")
